33d8bfadf60571cb3fdb7590f473363cbdeb0f1a
sajadj313 <sajad.jaward1@gmail.com>
2019-02-08 00:46:09 +0530

    added

---
+++ lib/models/vendor_settings.dart
@@ -0,0 +1,45 @@
+import 'package:suarabiz/common/common.dart';
+
+class VendorSettings{
+  String uid;
+  String businessName;
+  String businessDesc;
+  String fbURL;
+  dynamic location;
+  dynamic location2;
+  String whatsappNo;
+  String phoneNo;
+  String category;
+  bool isOnline;
+  bool isLoc1Def;
+  int credits;
+  String salesContact;
+  bool creditPolicy;
+  DateTime lastOnline;
+
+  
+  VendorSettings(this.uid){
+    isLoc1Def = true;
+    isOnline = false;
+    creditPolicy = false;
+    lastOnline = DateTime.now();
+  }
+
+  VendorSettings.fromJson(dynamic f){
+    uid = f['uid'];
+    businessName = f['businessName'];
+    businessDesc = f['businessDesc'];
+    fbURL = f['fbURL'];
+    location = f['location'];
+    location2 = f['location2'];
+    whatsappNo = f['whatsappNo'];
+    phoneNo = f['phoneNo'];
+    category = f['category'];
+    isOnline = f['isOnline'] == null ? false : f['isOnline'];
+    isLoc1Def = f['isLoc1Def'] == null ? true : f['isLoc1Def'];
+    credits = f['credits'] ?? initialCredit;
+    salesContact = f['salesContact'];
+    creditPolicy = f['creditPolicy'];
+    lastOnline = f['lastOnline'];
+  }
+}
