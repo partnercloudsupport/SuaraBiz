a9aee1098429c19e9df3732ef56dd49e3b41f5b2
sajadj313 <sajad.jaward1@gmail.com>
2019-02-10 21:56:10 +0530

    removed unused codes

--- lib/main.dart
+++ lib/main.dart
@@ -16,7 +16,7 @@ class SuaraBizApp extends StatelessWidget {
         theme: ThemeData(
           primarySwatch: Colors.green,
         ),
-        home: /*Login()*/ FutureBuilder(
+        home: FutureBuilder(
           future: FirebaseAuth.instance.currentUser(),
           builder: (context, snapShot) {
             if (snapShot.hasData) {
@@ -34,76 +34,10 @@ class SuaraBizApp extends StatelessWidget {
                   return screenToRender;
                 },
               );
-              //get the user role second
-              /*Firestore.instance
-                  .collection('salesagents')
-                  .document(loggedInUserId)
-                  .snapshots()
-                  .listen((data) {
-                SalesAgent loggedInAgent =
-                    SalesAgent.fromJson(data.data);
-                Widget toRoute;
-                if (loggedInAgent.role == 'sales') {
-                  toRoute = Sales();
-                } else {
-                  toRoute = Admin();
-                }
-
-                return toRoute;
-              })*//*.cancel()*/;
-
-              //return Sales();
             }
             return Login();
           },
-        ) //MyHomePage(title: 'Flutter Demo Home Page'),
+        )
         );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ),
-    );
   }
 }
