edea0293640fb5c3a65b2d8d08b5ec96d10b1784
sajadj313 <sajad.jaward1@gmail.com>
2019-02-10 11:58:57 +0530

    added a snackbar which can be used commonly to display errors

--- lib/common/common.dart
+++ lib/common/common.dart
@@ -1,3 +1,10 @@
+import 'package:flutter/material.dart';
+
 final int initialCredit = 3;
 
 final double bottomNavBarIconSize = 25.0;
+
+SnackBar buildErrorIndicatorSnackBar(String content) => SnackBar(
+      content: Text(content),
+      backgroundColor: Colors.red,
+    );
