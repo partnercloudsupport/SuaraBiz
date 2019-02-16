b9d486779d39f2d36ab7d218d640bbd17a05f0b1
sajadj313 <sajad.jaward1@gmail.com>
2019-02-16 12:25:38 +0530

    created a common widget to display remaining credits of sales agent and vendors

--- lib/common/common.dart
+++ lib/common/common.dart
@@ -9,5 +9,29 @@ SnackBar buildErrorIndicatorSnackBar(String content) => SnackBar(
       backgroundColor: Colors.red,
     );
 
-
 final String giveCreditsText = 'Give credits';
+
+Widget remainingCreditsWidget(int remainingCreditAmount, String text) =>
+    Container(
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(10.0),
+        color: Colors.grey.shade200,
+      ),
+      padding: EdgeInsets.all(10.0),
+      child: SizedBox(
+        width: 80.0,
+        child: Column(
+          children: <Widget>[
+            CircleAvatar(
+              radius: 30.0,
+              child: Text('$remainingCreditAmount'),
+            ),
+            Padding(padding: EdgeInsets.only(top: 10.0),),
+            Text(
+              text,
+              style: TextStyle(fontSize: 15.0),
+            )
+          ],
+        ),
+      ),
+    );
