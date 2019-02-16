39bdd1cf8f5149a1d911f5a2eba1eaf3e00d2267
sajadj313 <sajad.jaward1@gmail.com>
2019-02-16 22:49:13 +0530

    refurbished the functionalities

--- lib/screens/admin_screens/add_new_agent.dart
+++ lib/screens/admin_screens/add_new_agent.dart
@@ -9,12 +9,62 @@ class AddNewAgent extends StatefulWidget {
 }
 
 class _AddNewAgentState extends State<AddNewAgent> {
-  final TextEditingController _emailController = TextEditingController();
-  final TextEditingController _passwordController = TextEditingController();
-  final TextEditingController _retypePassController = TextEditingController();
-  final TextEditingController _nameController = TextEditingController();
-  final TextEditingController _creditController = TextEditingController();
   final GlobalKey<ScaffoldState> _scaffoldKey = GlobalKey();
+  final GlobalKey<FormState> _formKey = GlobalKey();
+  final SalesAgent _tempAgentDetailsHolder = SalesAgent.emptyObject();
+
+  String fieldValidator(String val) {
+    if (val.isEmpty) {
+      return 'Cannot be empty';
+    }
+    return null;
+  }
+
+  String passwordValidator(String val) {
+    if (val.isEmpty) {
+      return 'Cannot be empty';
+    }
+
+    if (_tempAgentDetailsHolder.password.isEmpty && val.isNotEmpty) {
+      return 'Must type and retype the password';
+    }
+
+    if (_tempAgentDetailsHolder.password.compareTo(val) != 0) {
+      return 'Password don\'t match';
+    }
+    return null;
+  }
+
+  void saveUser() async {
+    try {
+      _formKey.currentState.save();
+
+      // below steps should happen if the form is valid
+      if (_formKey.currentState.validate()) {
+        //create a user in firebase auth first and obtain the uid
+        var createdUser = await FirebaseAuth.instance
+            .createUserWithEmailAndPassword(
+                email: _tempAgentDetailsHolder.email,
+                password: _tempAgentDetailsHolder.password);
+
+        _tempAgentDetailsHolder.id = createdUser.uid;
+
+        //using the obtained uid, store the user in the firestore
+        Firestore.instance
+            .collection('salesagents')
+            .document(_tempAgentDetailsHolder.id)
+            .setData(_tempAgentDetailsHolder.toJson());
+
+        //close the screen
+        Navigator.of(context).pop(_tempAgentDetailsHolder);
+      }
+    } catch (error) {
+      _scaffoldKey.currentState.showSnackBar(SnackBar(
+        backgroundColor: Colors.red,
+        content: Text(error.message),
+      ));
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -23,97 +73,63 @@ class _AddNewAgentState extends State<AddNewAgent> {
       appBar: AppBar(
         title: Text('Add new Sales Agent'),
       ),
-      body: Column(
-        children: <Widget>[
-          ListTile(
-            title: Text('Name'),
-          ),
-          ListTile(
-            title: Container(
-              child: TextField(
-                controller: _nameController,
+      body: Padding(
+        padding: EdgeInsets.only(left: 15.0, right: 15.0),
+        child: Form(
+          key: _formKey,
+          child: ListView(
+            children: <Widget>[
+              TextFormField(
+                validator: fieldValidator,
+                decoration: InputDecoration(labelText: 'Name'),
+                onSaved: (val) {
+                  _tempAgentDetailsHolder.name = val;
+                },
               ),
-            ),
-          ),
-          ListTile(
-            title: Text('Email address'),
-          ),
-          ListTile(
-            title: Container(
-              child: TextField(
-                controller: _emailController,
+              TextFormField(
+                validator: fieldValidator,
+                decoration: InputDecoration(labelText: 'Email Address'),
+                keyboardType: TextInputType.emailAddress,
+                onSaved: (val) {
+                  _tempAgentDetailsHolder.email = val;
+                },
               ),
-            ),
-          ),
-          ListTile(
-            title: Text('Password'),
-          ),
-          ListTile(
-            title: Container(
-              child: TextField(
-                controller: _passwordController,
+              TextFormField(
+                validator: fieldValidator,
+                decoration: InputDecoration(labelText: 'Password'),
+                obscureText: true,
+                onSaved: (val) {
+                  _tempAgentDetailsHolder.password = val;
+                },
               ),
-            ),
-          ),
-          ListTile(
-            title: Text('Retype Password'),
-          ),
-          ListTile(
-            title: Container(
-              child: TextField(
-                controller: _retypePassController,
+              TextFormField(
+                validator: passwordValidator,
+                decoration: InputDecoration(labelText: 'Retype Password'),
+                obscureText: true,
               ),
-            ),
-          ),
-          ListTile(
-            title: Text('Credit Amount'),
-          ),
-          ListTile(
-            title: Container(
-              child: TextField(
-                keyboardType: TextInputType.number,
-                controller: _creditController,
+              TextFormField(
+                validator: fieldValidator,
+                decoration: InputDecoration(labelText: 'Credit Amount'),
+                keyboardType: TextInputType.numberWithOptions(
+                    decimal: false, signed: false),
+                onSaved: (val) {
+                  _tempAgentDetailsHolder.credits = val == null || val.trim() == '' ? 0 : int.parse(val);
+                },
               ),
-            ),
+            ],
           ),
-          Expanded(
-            child: Container(),
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: EdgeInsets.only(left: 15.0, right: 15.0),
+        child: RaisedButton(
+          color: Theme.of(context).primaryColor,
+          onPressed: saveUser,
+          child: Text(
+            'Save',
+            style: TextStyle(color: Colors.white),
           ),
-          ListTile(
-            title: RaisedButton(
-              color: Theme.of(context).primaryColor,
-              onPressed: () async {
-                try {
-                  var createdUser = await FirebaseAuth.instance
-                      .createUserWithEmailAndPassword(
-                          email: _emailController.text,
-                          password: _passwordController.text);
-                  var agent = SalesAgent(
-                      createdUser.uid,
-                      _nameController.text,
-                      _emailController.text,
-                      _passwordController.text,
-                      int.parse(_creditController.text));
-
-                  Firestore.instance
-                      .collection('salesagents')
-                      .document(agent.id)
-                      .setData(agent.toJson());
-                  Navigator.of(context).pop(agent);
-                } catch (error) {
-                  _scaffoldKey.currentState.showSnackBar(SnackBar(
-                    backgroundColor: Colors.red,
-                    content: Text(error.message),
-                  ));
-                }
-              },
-              child: Text(
-                'Save',
-                style: TextStyle(color: Colors.white),
-              ),
-            ),
-          )
-        ],
+        ),
       ),
     );
   }
