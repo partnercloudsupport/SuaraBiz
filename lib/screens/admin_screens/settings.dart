75c50eb23f09c0bcbbfe34cac71af7d39d3493fe
sajadj313 <sajad.jaward1@gmail.com>
2019-02-17 20:35:26 +0530

    implemented location removal and vendor offline functionality

--- lib/screens/admin_screens/settings.dart
+++ lib/screens/admin_screens/settings.dart
@@ -1,5 +1,7 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:suarabiz/common/common.dart';
+import 'package:firebase_database/firebase_database.dart';
 
 class AdminSettings extends StatefulWidget {
   @override
@@ -24,6 +26,25 @@ class _AdminSettingsState extends State<AdminSettings> {
       setState(() {
         _isCreditPolicyEnabled = creditPolicyVal;
       });
+    }
+  }
+
+  void removeLocations() async {
+    //remove locations node from firebase db
+    FirebaseDatabase.instance.reference().child('locations').remove();
+
+    //building a list of IDs that we need the isonline property to be false
+    var vendors =
+        await Firestore.instance.collection('vendorsettings').getDocuments();
+    var vendorIDs = [];
+
+    for (int i = 0; i < vendors.documents.length; i++) {
+      vendorIDs.add(vendors.documents[i].documentID);
+    }
+
+    //set vendorsettings isonline property to false in firestore
+    for(int i=0; i<vendorIDs.length; i++){
+      Firestore.instance.collection('vendorsettings').document(vendorIDs[i]).updateData({'isOnline':false});
     }
   }
 
@@ -58,9 +79,22 @@ class _AdminSettingsState extends State<AdminSettings> {
             title: Text('Remove location nodes'),
             subtitle: Text('This will affect all the vendors'),
             trailing: RaisedButton(
-              onPressed: (){},
+              onPressed: () async {
+                var shouldRemove = await showDialog(
+                    context: context,
+                    barrierDismissible: true,
+                    builder: (context) =>
+                        locationRemovalConfirmationDialog(context));
+
+                if (shouldRemove) {
+                  removeLocations();
+                }
+              },
               color: Colors.red,
-              child: Text('REMOVE',style: TextStyle(color: Colors.white),),
+              child: Text(
+                'REMOVE',
+                style: TextStyle(color: Colors.white),
+              ),
             ),
           )
         ],
